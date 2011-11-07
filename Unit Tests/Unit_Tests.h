3d2befc5f561f834c77c3cbed2dbe21d6ca3ca0d
Steven Holloway <impurist@gmail.com>
2011-11-07 16:34:31 +1100

    add unit test target

---
+++ Unit Tests/Unit_Tests.h
@@ -0,0 +1,13 @@
+//
+//  Unit_Tests.h
+//  Unit Tests
+//
+//  Created by Steven Holloway on 7/11/11.
+//  Copyright (c) 2011 Agile Impetus. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Unit_Tests : SenTestCase
+
+@end
---
+++ Unit Tests/Unit_Tests.m
@@ -0,0 +1,32 @@
+//
+//  Unit_Tests.m
+//  Unit Tests
+//
+//  Created by Steven Holloway on 7/11/11.
+//  Copyright (c) 2011 Agile Impetus. All rights reserved.
+//
+
+#import "Unit_Tests.h"
+
+@implementation Unit_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Unit Tests");
+}
+
+@end
